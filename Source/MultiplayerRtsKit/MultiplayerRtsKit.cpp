5485597145612cd8aa241a3df680035c4aef8f38
David Rud Pedersen <davidrudpedersen@gmail.com>
2024-08-30 09:58:35 +0200

    Install `AdvancedSessions` plugin

---
+++ Source/MultiplayerRtsKit/MultiplayerRtsKit.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MultiplayerRtsKit.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiplayerRtsKit, "MultiplayerRtsKit" );
